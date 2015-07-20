01e62fdcc33f22b75dfb4b3b1c9bca2a3f67633b
ms2sato <ms2.sato@gmail.com>
2015-07-20 14:21:37 +0900

    メール機能まで

--- app.rb
+++ app.rb
@@ -20,15 +20,19 @@ post '/confirm' do
   erb :confirm
 end
 
-get '/test' do
-  send_simple_message
+post '/send_mail' do
+  email = params[:email]
+  message = params[:message]
+  send_message(email, message)  
+
+  erb :thanks
 end
 
-def send_simple_message
+def send_message(email, message)
     RestClient.post "https://api:key-cabe4d08ae4cc3fab6b5946eb54f0123"\
       "@api.mailgun.net/v3/sandboxcebbc7038ab145f1968fcd1e04eb9142.mailgun.org/messages",
-      :from => "Mailgun Sandbox <postmaster@sandboxcebbc7038ab145f1968fcd1e04eb9142.mailgun.org>",
+      :from => "お問い合わせ <ms2.sato@gmail.com>",
       :to => "MasashiSato <ms2.sato@gmail.com>",
-      :subject => "Hello MasashiSato",
-      :text => "Congratulations MasashiSato, you just sent an email with Mailgun!  You are truly awesome!  You can see a record of this email in your logs: https://mailgun.com/cp/log .  You can send up to 300 emails/day from this sandbox server.  Next, you should add your own domain so you can send 10,000 emails/month for free."
+      :subject => "お問い合わせがきました！",
+      :text => "email: #{email}さんからお問い合わせがきました。本文: #{message}"
 end
